2df3a294649120fbc11875519fb0ecb8d4eb1313
Sakuiwa <mafuyunosennpuuki@gmail.com>
2015-05-12 16:14:35 +0900

    5/12
    5/12更新分

--- example/21_particleStartpoint/src/ofApp.cpp
+++ example/21_particleStartpoint/src/ofApp.cpp
@@ -8,12 +8,18 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    for (int i = 0; i < NUM ; i++) {
+        particle[i].addForce(ofVec2f(0, 1.0));
+        particle[i].update();
+        particle[i].boundOffwalls();
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    for (int i = 0; i < NUM; i++){
+    particle[i].draw();
+    }
 }
 
 //--------------------------------------------------------------
@@ -43,7 +49,19 @@ void ofApp::mousePressed(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    for (int i = 0; i < NUM; i++){
+    particle[i].position.set(x, y);
+    particle[i].velocity.set(0, 0);
+    float length = ofRandom(20);
+    float angle = ofRandom(2.0 * PI);
+    float rx = length * cos(angle);
+    float ry = length * sin(angle);
+    ofVec2f force = ofVec2f(length * cos(angle), length * sin(angle));
+    particle[i].addForce(force);
+    //particle[i].mass = 10.0;
+    //particle[i].acceleration.set(ofRandom(-10, 10), ofRandom(-20, 0));
+    //particle[i].addForce(ofVec2f(ofRandom(10, 10), ofRandom(10, 10)));
+    }
 }
 
 //--------------------------------------------------------------
